e50f6c5e8db8548b4748c32175da40eb320cd5f4
biscuegoheimochi <biscue20.goheimochi26@gmail.com>
2026-01-10 16:17:25 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+// Configuration File
+// Update this URL whenever you redeploy your Google Apps Script Web App.
+const CONFIG = {
+    API_URL: "https://script.google.com/macros/s/AKfycbx7NUviiiV8PFw2jJoeVHhcikCay2TV_QFcqOxWNKEZPI7el4h1UFySs7gsv46YOgp_SA/exec"
+};
+
+// Global Helper to get URL easily if needed, though CONFIG.API_URL is sufficient.
+// This file should remain small.
