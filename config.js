f85f3ac5148f7d45587d6a54cc7c4c144b5ad733
biscuegoheimochi <biscue20.goheimochi26@gmail.com>
2026-01-10 16:27:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,10 @@
 // Configuration File
 // Update this URL whenever you redeploy your Google Apps Script Web App.
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbz1HQLfwcriavaREiFTvnkaPDaULOMdMy_-k56Fpm8cS0DjkLaGLbfoQcPk9foUDiYjig/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbyW2ZNZ9i6xeGDeWhFnHYL6sk06XhGZWkLui0AKxB5o1cVWBoXlo5ezalo4HZUqZxl8ig/exec"
 };
 
 // Global Helper to get URL easily if needed, though CONFIG.API_URL is sufficient.
 // This file should remain small.
+
 
